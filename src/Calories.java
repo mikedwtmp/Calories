619a618e5d34162b8c1170c6a846d709ed30fcec
“Michael <mdreis@luc.edu>
2023-01-19 16:23:43 -0600

    Finished calculation outputs

--- src/Calories.java
+++ src/Calories.java
@@ -26,24 +26,48 @@
     Author: C. Fulton
 */
 import java.util.Scanner; //Scanner Class imported
+//import java.lang.Integer;
+
+import javax.swing.event.SwingPropertyChangeSupport;
 
 public class Calories {
     public static void main(String[] args) throws Exception {
 
         //DECLARATIONS (Declare your variables below including your Scanner Object)
-
+        int userCups;
+        String userName;
+        int totalCalories;
+        int totalFatCalories;
+        int totalGrams;
+        final int CALORIES_PER_CUP = 150;
+        final int CALORIES_FROM_FAT = 54;
+        final int GRAMS_PER_CUP = 30;
 
         //INTRODUCTION
         System.out.println("Welcome to the Garrett's Popcorn Calorie Tracker!  This program will allow you to enter the total number of cups of Garrett's popcorn and provide you with the total number of calories consumed, the breakdown of calories from fat and the total number of grams per cup consumed.");
         System.out.println(" ");
 
         //INPUT AND OUTPUT (Use wording found in .gif video on README.md )
-
+        Scanner scn = new Scanner(System.in);
+        System.out.println("To get started, please enter the total number of popcorn cups consumed: ");
+        userCups = Integer.parseInt(scn.nextLine());
+        System.out.println("Please enter your name: ");
+        userName = scn.nextLine();
 
         //MAKE CALCULATIONS
+        totalCalories = userCups * CALORIES_PER_CUP;
+        totalFatCalories = userCups * CALORIES_FROM_FAT;
+        totalGrams = userCups * GRAMS_PER_CUP;
 
 
         //OUTPUT DATA TO CONSOLE
+        System.out.println(userName + " your caloric intake of Garrett's Popcorn is listed below:");
+        System.out.println("*****************************************************************************");
+        System.out.println(userCups + " cup(s) equtes to " + totalCalories + " calories");
+        System.out.println(userCups + " cup(s) equates to " + totalGrams + " grams consumed");
+        System.out.println("Of the " + totalCalories + " calories - " + totalFatCalories + " are from fat");
+
+        scn.close();
 
 
     }
